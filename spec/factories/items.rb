37197a7aa9185fbd8fbf5dfd87ccb6a158b5047e
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-02 11:31:21 +0900

    itemテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    product_name      {Faker::Lorem.sentence}
+    explanation       {2}
+    category_id       {2}
+    condition_id      {2}
+    price             {"9999"}
+    send_cost_id      {2}
+    prefecture_id     {2}
+    send_day_id       {2}
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
