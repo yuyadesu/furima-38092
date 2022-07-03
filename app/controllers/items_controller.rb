5760d5861d945b40fb5b2ca417932fa9b4ae62cf
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-03 14:26:08 +0900

    表品データの有無でダミー表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    # @items = Item.all
-    # @items = Item.order("created_at DESC")
+    @items = Item.order("created_at asc")
   end
 
   def new
