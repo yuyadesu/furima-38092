a36d34ca5831b1969011ea566430a8d0995f929a
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-04 15:10:51 +0900

    ログインの有無による違い

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order("created_at desc")
