edbd6b3239496b4002206fc2ff62ca50dbc90386
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-04 14:35:17 +0900

    ログインの有無の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
       redirect_to action: :index
     end
   end
