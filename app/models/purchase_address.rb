c3415b62db489f36f739f35faff3d8c9ae3a4c00
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-05 17:32:59 +0900

    formオブジェクト作成

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,11 +1,16 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :price, :user_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :item_id
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, purchase_id: purchase.id)
+  end
 end
