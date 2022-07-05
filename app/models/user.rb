2cdba504072c8846f1da7dc0c366d551034cc5af
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-05 15:54:20 +0900

    モデルのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :purchases
 
   validates :nickname, presence: true
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
