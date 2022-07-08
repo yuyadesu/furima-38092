400fedc2bb4df961f927eabe28e6035352facb0c
Lister10 <106854019+Lister10@users.noreply.github.com>
2022-07-08 15:20:23 +0900

    購入情報の表示 sold out

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: :index
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       pay_item
@@ -18,9 +19,6 @@ class PurchasesController < ApplicationController
   end
 
   private
-  def item_params
-    params.require(:item).permit(:image, :product_name, :explanation, :category_id, :condition_id, :price, :send_cost_id, :prefecture_id, :send_day_id).merge(user_id: current_user.id, item_id: params[:item_id])
-  end
 
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
@@ -33,5 +31,18 @@ class PurchasesController < ApplicationController
       card: purchase_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if @item.purchase.present?
+      redirect_to root_path
+    end
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 end
